e333fb79b879a2ff0015057c93c0873204787674
totoxb8 <artaud.thomas05@gmail.com>
2026-05-25 12:55:37 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RoutePlanner from './route-planner.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RoutePlanner />
+  </React.StrictMode>,
+)
